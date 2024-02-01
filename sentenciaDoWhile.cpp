f4d526fbb9cd8aa71e6c41a87878649a84b6241d
LonelyMoon <87772477+LonelyMo0n@users.noreply.github.com>
2024-01-31 19:35:17 -0600

    Add files via upload

---
+++ sentenciaDoWhile.cpp
@@ -0,0 +1,21 @@
+/*Sentencia Do While*/
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main(){
+	int i;
+	i = 10;
+	
+	do{
+	cout<<i<<endl;
+	i--; //aumenta el iterador de uno en uno	
+		
+	}while(i>=1);
+	
+	
+	system("pause");
+	return 0;
+}
+//Do While, orimero actua, luego piensa
+//While, piensa y luego actua
---
+++ sentenciaFOR.cpp
@@ -0,0 +1,20 @@
+/*La sentencia for
+
+    for(expr1; expresion logica; expr2){
+        conjunto de instrucciones;
+    }
+*/
+
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main(){
+	
+	for(int i=1; i<=10; i++){
+		cout<<i<<endl;
+	}	 
+	
+	system("pause");
+	return 0;
+}
